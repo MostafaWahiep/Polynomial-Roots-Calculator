1ef03d0508ca5a3f997ee6edb7b2ebd4ade6f300
MostafaWahiep <112508570+MostafaWahiep@users.noreply.github.com>
2023-01-29 17:41:22 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,64 +14,64 @@ int counter; // To maintain a count of the number of solutions that have been pr
 
 //functions' prototype
 
-complex<double> SolveFor(complex<double> x, double coefficients[], int n);
+complex<double> SolveFor(complex<double> x, double coefficients[], int degree);
 /*
 to solve in the input equation with x
 returns F(x)
 */
 
-complex<double> *FillIntialGuesses(double coefficients[], int n);
+complex<double> *FillIntialGuesses(double coefficients[], int degree);
 /*
 It builds a dynamic array filled with the starting points to iterate over
 The basic idea is to evenly distribute the points on a circle
 */
 
-void iteration(double coefficients[], complex<double> *initial, int n);
+void iteration(double coefficients[], complex<double> *initial, int degree);
 /*
 It applies Durand-kerner method, iterating over the initials till it stop have the solutions
 */
 
-bool CheckValidEquation(string s);
+bool CheckValidEquation(string &input_polynomial);
 /*
 checks the validity of the input string whether it follow the criteria or not to operate with the equation correctly
 */
 
-vector<pair<double, int>> Operatecoefficients(string s, int *o);
+vector<pair<double, int>> Operatecoefficients(string &input_polynomial, int *degree);
 /*
 returns a vector of pairs; first is the coefficient of order of the second which is the power
 example (2.3,7) is 2.3x^7
 */
 
-void PrintSolutions(complex<double> *initial, int n);
+void PrintSolutions(complex<double> *initial, int degree);
 
 int main()
 {
-	int n = 0; //assume that the degree is zero
-	string s;
-	getline(cin, s, '\n');
+	int degree = 0; //assume that the degree is zero
+	string input_polynomial;
+	getline(cin, input_polynomial, '\n');
 
-	if ((!CheckValidEquation(s))) //check the input equation
+	if ((!CheckValidEquation(input_polynomial))) //check the input equation
 	{
 		cout << "error";
 		return 0;
 	}
 
-	vector<pair<double, int>> poly = Operatecoefficients(s, &n);
-	double *coefficients = new double[n + 1]{ 0 };
+	vector<pair<double, int>> poly = Operatecoefficients(input_polynomial, &degree);
+	double *coefficients = new double[degree + 1]{ 0 };
 
 	//filling the coefficients
 	for (int i = 0; i < poly.size(); i++)
-		coefficients[n - poly[i].second] += poly[i].first;
+		coefficients[degree - poly[i].second] += poly[i].first;
 
 
     //printing the coefficients
     cout << "Coefficients: ";
-	for (int i = 0; i < n + 1; i++)
+	for (int i = 0; i < degree + 1; i++)
 		cout << coefficients[i] << " ";
 	cout << endl;
 
 	//constant equations have no solution
-	if (n == 0)
+	if (degree == 0)
 		cout << "no solution";
 	else
 	{
@@ -80,23 +80,23 @@ int main()
 		if (coefficients[0] != 1)
 		{
 			double y = coefficients[0];
-			for (int i = 0; i < n + 1; i++)
+			for (int i = 0; i < degree + 1; i++)
 			{
 				coefficients[i] /= y;
 			}
 		}
 
         //reducing the equation: x^3 + 2x^2 = x^2(x+2) and solve for x+2
-		while (coefficients[n] == 0)
+		while (coefficients[degree] == 0)
 		{
 			cout << "X" << setw(2) << left << ++counter << " = 0" << endl;
-			n--;
+			degree--;
 		}
 
-		if (n != 0)
+		if (degree != 0)
 		{
-			complex<double> *intial = FillIntialGuesses(coefficients, n);
-			iteration(coefficients, intial, n);
+			complex<double> *intial = FillIntialGuesses(coefficients, degree);
+			iteration(coefficients, intial, degree);
 			delete[] intial;
 		}
 	}
@@ -104,10 +104,10 @@ int main()
 	delete[] coefficients;
 }
 
-complex<double> SolveFor(complex<double> x, double coefficients[], int n)
+complex<double> SolveFor(complex<double> x, double coefficients[], int degree)
 {
 	complex<double> temp = coefficients[0];
-	for (int i = 1; i <= n; i++)
+	for (int i = 1; i <= degree; i++)
 	{
 		temp = x *temp + coefficients[i];
 	}
@@ -115,13 +115,13 @@ complex<double> SolveFor(complex<double> x, double coefficients[], int n)
 	return temp;
 }
 
-complex<double> *FillIntialGuesses(double coefficients[], int n)
+complex<double> *FillIntialGuesses(double coefficients[], int degree)
 {
-	complex<double> *intial = new complex < double>[n];
-	double a0 = coefficients[n], an = coefficients[0];
-	double r = pow(abs(a0 / an), 1.0 / n);
-	double theta = (2 *M_PI) / n;
-	for (int i = 0; i < n; i++)
+	complex<double> *intial = new complex < double>[degree];
+	double a0 = coefficients[degree], an = coefficients[0];
+	double r = pow(abs(a0 / an), 1.0 / degree);
+	double theta = (2 *M_PI) / degree;
+	for (int i = 0; i < degree; i++)
 	{
 		complex<double> temp(r* cos(i *theta + theta / 4), r* sin(i *theta + theta / 4)); //
 		intial[i] = temp;
@@ -130,29 +130,29 @@ complex<double> *FillIntialGuesses(double coefficients[], int n)
 	return intial;
 }
 
-void iteration(double coefficients[], complex<double> *initial, int n)
+void iteration(double coefficients[], complex<double> *initial, int degree)
 {
 	int itr = 0;
 	for (; itr < 300; itr++)
 	{
-		for (int i = 0; i < n; i++)
+		for (int i = 0; i < degree; i++)
 		{
 			complex<double> temp = 1;
-			for (int j = 0; j < n; j++)
+			for (int j = 0; j < degree; j++)
 			{
 				if (j == i)
 					continue;
 				temp *= initial[i] - initial[j];
 			}
 
-			initial[i] = initial[i] - SolveFor(initial[i], coefficients, n) / temp;
+			initial[i] = initial[i] - SolveFor(initial[i], coefficients, degree) / temp;
 		}
 	}
 
 	//cout << "It takes "<< itr << " Iterations\n";
 
 	//some solutions would have small imaginary due to floating point imprecision so make them real
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < degree; i++)
 	{
 		if (initial[i].imag() < 1e-5 && initial[i].imag() > (-1 * 1e-5))
 		{
@@ -162,19 +162,19 @@ void iteration(double coefficients[], complex<double> *initial, int n)
 	}
 
     //sorting with respect to the absolute of the imaginary part so the real solution are in the begining
-	sort(initial, initial + n, [](complex < double>
+	sort(initial, initial + degree, [](complex < double>
 		const &a, complex < double>const &b)->bool{
 
 		return abs(a.imag()) < abs(b.imag());
 
 	});
 
-	PrintSolutions(initial,n);
+	PrintSolutions(initial, degree);
 }
 
-void PrintSolutions(complex<double> *initial, int n)
+void PrintSolutions(complex<double> *initial, int degree)
 {
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < degree; i++)
 	{
 		cout << "X" << setw(2) << left << ++counter << " = " << setw(10) << fixed <<setprecision(5) << initial[i].real();
 		if (initial[i].imag() > 0)
@@ -188,96 +188,95 @@ void PrintSolutions(complex<double> *initial, int n)
 	}
 }
 
-bool CheckValidEquation(string s)
+bool CheckValidEquation(string &input_polynomial)
 {
 	int i = 0;
-	if (s[0] == '-')
+	if (input_polynomial[0] == '-')
 		i++;
 
-	for (; i < s.size(); i++)
+	for (; i < input_polynomial.size(); i++)
 	{
 	    //contains only allowed characters
-		if (!(s[i] == 'x' || s[i] == '+' || s[i] == '-' || s[i] == '^' || (s[i] >= '0' && s[i] <= '9') || s[i] == ' ' || s[i] == '.'))
+		if (!(input_polynomial[i] == 'x' || input_polynomial[i] == '+' || input_polynomial[i] == '-' || input_polynomial[i] == '^' || (input_polynomial[i] >= '0' && input_polynomial[i] <= '9') || input_polynomial[i] == ' ' || input_polynomial[i] == '.'))
 			return false;
         //after the power is only a number
-		if (s[i] == '^' && !(s[i + 1] >= '0' && s[i + 1] <= '9'))
+		if (input_polynomial[i] == '^' && !(input_polynomial[i + 1] >= '0' && input_polynomial[i + 1] <= '9'))
 			return false;
         //after x is space or power or an end line
-		if (s[i] == 'x' && !(s[i + 1] == ' ' || s[i + 1] == '^' || s[i + 1] == '\0'))
+		if (input_polynomial[i] == 'x' && !(input_polynomial[i + 1] == ' ' || input_polynomial[i + 1] == '^' || input_polynomial[i + 1] == '\0'))
 			return false;
         //after the sign is space and after the space is x or a number
-		if ((s[i] == '+' || s[i] == '-') && ((s[i + 1] != ' ' || s[i - 1] != ' ') || !(s[i + 2] == 'x' || (s[i + 2] >= '0' && s[i + 2] <= '9'))))
+		if ((input_polynomial[i] == '+' || input_polynomial[i] == '-') && ((input_polynomial[i + 1] != ' ' || input_polynomial[i - 1] != ' ') || !(input_polynomial[i + 2] == 'x' || (input_polynomial[i + 2] >= '0' && input_polynomial[i + 2] <= '9'))))
 			return false;
 	}
 
 	return true;
 }
 
-vector<pair<double, int>> Operatecoefficients(string s, int *o)
+vector<pair<double, int>> Operatecoefficients(string &input_polynomial, int *degree)
 {
 	vector<pair<double, int>> poly;
-	int terms = count(s.begin(), s.end(), '+') + count(s.begin(), s.end(), '-') + 1;
+	int terms = count(input_polynomial.begin(), input_polynomial.end(), '+') + count(input_polynomial.begin(), input_polynomial.end(), '-') + 1;
 	int index = 0;
 	bool sign = false;	// true for -ve false for +ve
-	if (s[0] == '-')
+	if (input_polynomial[0] == '-')
 	{
 		index += 1;
 		sign = true;
 		terms--;
 	}
 
-	while (terms-- && index < s.size())
+	while (terms-- && index < input_polynomial.size())
 	{
 		double coefficient = 1; //a default for coefficient is 1: x^3 is 1
 		int power = 0;//a default for pwoer is 0: 2 is 2x^0
 
 		//check for the sign and jump
-		if (s[index] == '-')
+		if (input_polynomial[index] == '-')
 		{
 			sign = true;
 			index += 2;
 		}
-		else if (s[index] == '+')
+		else if (input_polynomial[index] == '+')
 		{
 			sign = false;
 			index += 2;
 		}
 
         //extract the coefficients
-		if ((s[index] >= '0' && s[index] <= '9') && index < s.size())
+		if ((input_polynomial[index] >= '0' && input_polynomial[index] <= '9') && index < input_polynomial.size())
 		{
 			int y = index;
-			while ((s[index + 1] >= '0' && s[index + 1] <= '9') || s[index + 1] == '.') //find the last number
+			while ((input_polynomial[index + 1] >= '0' && input_polynomial[index + 1] <= '9') || input_polynomial[index + 1] == '.') //find the last number
 			{
 				index++;
 			}
 
-			coefficient = stod(s.substr(y, index - y + 1));
+			coefficient = stod(input_polynomial.substr(y, index - y + 1));
 			index++;
 		}
 
         //the same for extracting the power if exists
-		if (s[index] == 'x' && index < s.size())
+		if (input_polynomial[index] == 'x' && index < input_polynomial.size())
 		{
-			if (s[index + 1] == '^')
+			if (input_polynomial[index + 1] == '^')
 			{
 				index += 2;
 				int y = index;
-				while ((s[index + 1] >= '0' && s[index + 1] <= '9'))
+				while ((input_polynomial[index + 1] >= '0' && input_polynomial[index + 1] <= '9'))
 				{
 					index++;
 				}
 
-				power = stoi(s.substr(y, index - y + 1));
+				power = stoi(input_polynomial.substr(y, index - y + 1));
 			}
 			else
 				power = 1;
 		}
 
         //keeping track of the greatest power to know the degree
-		if (power > *o)
-			*
-			o = power;
+		if (power > *degree)
+			*degree = power;
 
         //jump to the next term in the equation
 		index += 2;
